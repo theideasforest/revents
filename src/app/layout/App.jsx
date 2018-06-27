fb980c83abf2d414eca0aa899eec056d2ea6a77c
Mark White <mark@theideasforest.com>
2018-06-27 14:41:01 +0100

    End of Section 3

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import { Container  } from 'semantic-ui-react';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar';
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <h1>Re-vents</h1>
-
-      </div>
+    return (      
+        <div>
+          <NavBar/>
+          <Container className="main">
+            <EventDashboard/>
+          </Container>              
+        </div>        
     );
   }
 }
