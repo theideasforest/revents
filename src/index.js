c5902a5169bcfd4c2d5a00e1a4df15206fd5d804
Mark White <mark@theideasforest.com>
2018-07-02 13:15:52 +0100

    End of Section 7

--- src/index.js
+++ src/index.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
-import 'semantic-ui-css/semantic.min.css';
-import './index.css';
-import App from './app/layout/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import "semantic-ui-css/semantic.min.css";
+import "./index.css";
+import App from "./app/layout/App";
+import registerServiceWorker from "./registerServiceWorker";
+import { configureStore } from "./app/store/configureStore";
 
-const rootEl = document.getElementById('root');
+const store = configureStore();
+
+const rootEl = document.getElementById("root");
 
 let render = () => {
-    ReactDOM.render(
+  ReactDOM.render(
+    <Provider store={store}>
     <BrowserRouter>
-        <App />
+      <App />
     </BrowserRouter>
-    , rootEl)
-}
+    </Provider>,
+    rootEl
+  );
+};
 
 if (module.hot) {
-    module.hot.accept('./app/layout/App', () => {
-        setTimeout(render)
-    })
+  module.hot.accept("./app/layout/App", () => {
+    setTimeout(render);
+  });
 }
 
 render();
